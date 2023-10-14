ba6a0ec8054e30e55106a43d6db843b92ec97325
serafima8 <147659413+serafima8@users.noreply.github.com>
2023-10-14 23:37:39 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1.Треугольник задан координатами своих вершин. Найти периметр и площадь треугольника.
+            //double dlina1, dlina2, dlina3,P,S;
+            //Console.Write("x1=");
+            //double x1 = double.Parse(Console.ReadLine());
+            //Console.Write("y1=");
+            //double y1 = double.Parse(Console.ReadLine());
+            //Console.Write("x2=");
+            //double x2 = double.Parse(Console.ReadLine());
+            //Console.Write("y2=");
+            //double y2 = double.Parse(Console.ReadLine());
+            //Console.Write("x3=");
+            //double x3 = double.Parse(Console.ReadLine());
+            //Console.Write("y3=");
+            //double y3 = double.Parse(Console.ReadLine());
+            //dlina1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            //dlina2 = Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2));
+            //dlina3 = Math.Sqrt(Math.Pow(x3 - x1, 2) + Math.Pow(y3 - y1, 2));
+            //P = 0;
+            //P = dlina1 + dlina2 + dlina3;
+            //Console.WriteLine($"P={P}");
+            //S = 0;
+            //S = Math.Sqrt(P * (P - dlina1) * (P - dlina2) * (P - dlina3));
+            //Console.WriteLine($"S={S}");
+
+
+            //Задача 2.Стоимость поездки на автомобиле(ввод: s – расстояние, b – расход бензина на 100 км, c - цена бензина за 1 литр).
+            //int V, Cost;
+            //Console.Write("Введите расстояние:");
+            //int s = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите расход бензина на 100 км:");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите цену бензина за 1 литр:");
+            //int c = Convert.ToInt32(Console.ReadLine());
+            //V = (s / 100) * b;
+            //Console.WriteLine($"Объем топлива={V}");
+            //Cost = V * c;
+            //Console.WriteLine($"Стоимость поездки на автомобиле={Cost}");
+
+
+            //Задача 3. Известна цена 1 кг конфет, печенья и яблок. Найти стоимость всей покупки, если купили x кг конфет, у кг печенья и z кг яблок.
+            //Console.Write("Введите цену конфет:");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("Введите цену печенья:");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.Write("Введите цену яблок:");
+            //double c = double.Parse(Console.ReadLine());
+            //Console.Write("Введите кол-во конфет в кг:");
+            //double x = double.Parse(Console.ReadLine());
+            //Console.Write("Введите кол-во печенья в кг:");
+            //double y = double.Parse(Console.ReadLine());
+            //Console.Write("Введите кол-во яблок в кг:");
+            //double z = double.Parse(Console.ReadLine());
+            //double Cost = (a * x) + (b * y) + (c * z);
+            //Console.WriteLine($"Стоимость всей покупки:={Cost}");
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+        }
+    }
+}
